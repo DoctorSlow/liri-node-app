db59fbaf466327b55673975590a15ef53d9b1c57
Jeremy J. Lee <jeremyjlee@Jeremys-MacBook-Pro-2.local>
2018-03-28 00:24:13 -0700

    fixed spotify Artist name error, twitter data overflow, and do-what-it-says functionality

--- liri.js
+++ liri.js
@@ -17,33 +17,41 @@ var command = process.argv[2];
 var inputName = process.argv[3];
 // var inputName = inputName.replace(' ', '+');
 
+if (command != undefined) {
+    switchCase(command, inputName)
+};
 //setting up a switch-case statement to accommodate multiple commands
-switch (command) {
-    case "my-tweets":
-        myTweets();
-        break;
+//wrap in a function w/ command and input arguments
+function
+switchCase(command, inputName) {
+    switch (command) {
+        case "my-tweets":
+            myTweets();
+            break;
 
-    case "spotify-this-song":
-        spotifyThisSong();
-        break;
+        case "spotify-this-song":
+            spotifyThisSong(inputName);
+            break;
 
-    case "movie-this":
-        movieThis();
-        break;
+        case "movie-this":
+            movieThis(inputName);
+            break;
 
-    case "do-what-it-says":
-        doWhatItSays();
-}
+        case "do-what-it-says":
+            doWhatItSays();
+    };
+};
 
 // `node liri.js my-tweets`
 // Show your last 20 tweets and when they were created at in your terminal/bash window.
 function myTweets() {
     var params = {
         screen_name: 'doctorslow',
+        count: 20,
     };
     client.get('statuses/user_timeline/', params, function (error, tweets, response) {
         if (!error) {
-            console.log(tweets);
+            console.log(tweets[0].text);
         }
     });
 }
@@ -52,7 +60,7 @@ function myTweets() {
 // function spotifyThisSong() {}
 // `node liri.js spotify-this-song '<song name here>'`
 // * If no song is provided then your program will default to "The Sign" by Ace of Base.
-function spotifyThisSong() {
+function spotifyThisSong(inputName) {
     spotify.search({
         type: 'track',
         query: inputName,
@@ -60,7 +68,7 @@ function spotifyThisSong() {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        console.log("Artist: " + data.tracks.items[0].artists.name);
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
         console.log("Song: " + data.tracks.items[0].name);
         console.log("Preview link: " + data.tracks.items[0].preview_url);
         console.log("Album: " + data.tracks.items[0].album.name);
@@ -100,11 +108,12 @@ function doWhatItSays() {
         // Then split it by commas (to make it more readable)
         var dataArr = data.split(",");
         // We will then re-display the content as an array for later use.
-        console.log(dataArr);
-        var doItInput = console.log("node liri.js " + dataArr[0] + " " + dataArr[1]);
+        // console.log(dataArr);
+        // console.log(dataArr[0]);
+
+        switchCase(dataArr[0], dataArr[1]);
     });
 
-    function doIt(doItInput) {}
 };
 
 // ### BONUS
@@ -112,4 +121,4 @@ function doWhatItSays() {
 // * Make sure you append each command you run to the `log.txt` file. 
 // * Do not overwrite your file each time you run a command.
 
-console.log(process.argv);
+// console.log(process.argv);
